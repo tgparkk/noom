639369b5a0defb4a138810b65eeba2a1065e403b
tgparkk <sttgpark@gmail.com>
2022-08-23 22:53:07 +0900

    chapter2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 //console.log("hello");
 
-
+import http from "http";
+import WebSocket from "ws";
 import express from 'express';
 
 const app = express();
@@ -9,7 +10,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.rendirect("/"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+        console.log(`${message}`)
+    });
+    socket.send("hello!");
+})
+
+server.listen(3000, handleListen);
+
+
