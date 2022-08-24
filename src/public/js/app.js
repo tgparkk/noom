5a500c6cf16d26cde484a1aa6c1acc27ba1dabe0
tgparkk <sttgpark@gmail.com>
2022-08-24 21:45:35 +0900

    3장

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,6 @@
-
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -6,13 +8,39 @@ socket.addEventListener("open", () => {
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("Just got this:", message.data, "from the server");
+    //console.log("Just got this:", message.data, "from the server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server");
 })
 
-setTimeout(() => {
-    socket.send("hello from browser!");
-}, 5000);
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    //socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    // socket.send({
+    //     type: "nickname",
+    //     payload: "input.value"
+    // });
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
